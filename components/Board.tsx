c35c6683399a3e62b9e83df862e21218a754437b
Cristina-tmp <cristinacommissions@gmail.com>
2023-06-07 18:24:21 -0400

    Finished dnd functionality

--- components/Board.tsx
+++ components/Board.tsx
@@ -13,10 +13,14 @@ import {
 type Props = {};
 
 const Board = (props: Props) => {
-  const [board, getBoard] = useBoardStore((state) => [
-    state.board,
-    state.getBoard,
-  ]);
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ]
+  );
 
   // fetch board data and use it everywhere
   useEffect(() => {
@@ -25,10 +29,81 @@ const Board = (props: Props) => {
 
   //let go of the dnd operation
   const handldeOnDragEnd = (result: DropResult) => {
-    //console.log(result);
+    const { destination, source, type } = result;
+
+    if (!destination) return;
+
+    //handle column drag
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      //remove the index from source
+      const [removed] = entries.splice(source.index, 1);
+      //push the removed col to the destination index in the correct position
+      entries.splice(destination.index, 0, removed);
+      const reArrangeCols = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: reArrangeCols,
+      });
+    }
+
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const endColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const endCol: Column = {
+      id: endColIndex[0],
+      todos: endColIndex[1].todos,
+    };
+
+    if (!startCol || !endCol) return;
+    if (source.index === destination.index && startCol === endCol) return;
+
+    const newTodos = startCol.todos;
+    const [removedTodo] = newTodos.splice(source.index, 1);
+
+    //drag in same col
+    if (startCol.id === endCol.id) {
+      newTodos.splice(destination.index, 0, removedTodo);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns.set(newCol.id, newCol));
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      //drag task in another col
+      const newEndTodos = Array.from(endCol.todos);
+      newEndTodos.splice(destination.index, 0, removedTodo);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(endCol.id, {
+        id: endCol.id,
+        todos: newEndTodos,
+      });
+
+      //update db
+
+      updateTodoInDB(removedTodo, endCol.id);
+
+      setBoardState({ ...board, columns: newColumns });
+    }
   };
 
-  console.log(board);
+  //console.log(board);
   return (
     <>
       <DragDropContext onDragEnd={handldeOnDragEnd}>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,12 +1,14 @@
 import { create } from "zustand";
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
-
+import { databases } from "@/appwrite";
 // everything contained inside the board
 //go to typings and create a Board type
 interface BoardState {
   //of type Board
   board: Board;
   getBoard: () => void;
+  setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 //initial
@@ -21,5 +23,18 @@ export const useBoardStore = create<BoardState>((set) => ({
     const board = await getTodosGroupedByColumn();
     //set global state
     set({ board });
+  },
+
+  setBoardState: (board) => set({ board }),
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
   },
 }));
